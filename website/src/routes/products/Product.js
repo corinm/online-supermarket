2220eeca2e6a71ebd7eac522512ffe4e41c328ba
Corin Mulliss <corin.mulliss@gmail.com>
2020-04-13 18:31:56 +0100

    Improved products layout

--- website/src/routes/products/Product.js
+++ website/src/routes/products/Product.js
@@ -1,19 +1,29 @@
-import React from 'react'
-import { Card } from 'antd'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Card, Statistic } from "antd";
+import { Link } from "react-router-dom";
 
 const Product = ({ id, name, price, description, image, rating }) => {
   return (
     <Link to={`/products/${id}`}>
       <Card
         hoverable
-        style={{ width: 240 }}
-        cover={<img alt={name} src={image} />}
+        cover={<img alt={name} src={image} style={{ height: 240 }} />}
       >
-        <Card.Meta title={name} description={description} />
+        <Card.Meta
+          title={
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                {name}
+              </div>
+              <Statistic value={price} prefix="£" precision={2} />
+            </div>
+          }
+          // description={description}
+          // style={{ height: 60 }}
+        />
       </Card>
     </Link>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
--- website/src/routes/products/Products.js
+++ website/src/routes/products/Products.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import Product from './Product'
-import { Col, Row } from 'antd'
+import React from "react";
+import Product from "./Product";
+import { Col, Row } from "antd";
 
-import { useLoadProducts } from '../../services'
+import { useLoadProducts } from "../../services";
 
 const Products = () => {
-  const products = useLoadProducts()
+  const products = useLoadProducts();
 
   return (
     <div className="Products">
-      <Row gutter={16}>
+      <Row gutter={[16, 16]} justify="space-around">
         {products.map((product, i) => (
-          <Col span={8} key={i}>
+          <Col xs={2} sm={24} md={8} lg={6} xl={6} key={i}>
             <Product {...product} />
           </Col>
         ))}
       </Row>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
