8bab1c78b51ab05b75e4f9bf505738b18a6ec21c
Corin Mulliss <corin.mulliss@gmail.com>
2020-04-18 16:43:35 +0100

    Added quantity and add to product card

--- website/src/routes/products/Product.js
+++ website/src/routes/products/Product.js
@@ -1,28 +1,38 @@
-import React from "react";
-import { Card, Statistic } from "antd";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Card, Statistic, InputNumber, Button } from "antd";
+
+const AddToBasket = ({ id }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const onAddClick = () => console.log(id, quantity);
+
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <InputNumber value={quantity} onChange={setQuantity} />
+      <Button type="primary" onClick={onAddClick}>
+        Add
+      </Button>
+    </div>
+  );
+};
 
 const Product = ({ id, name, price, description, image, rating }) => {
   return (
-    <Link to={`/products/${id}`}>
-      <Card
-        hoverable
-        cover={<img alt={name} src={image} style={{ height: 240 }} />}
-      >
-        <Card.Meta
-          title={
+    <Card cover={<img alt={name} src={image} style={{ height: 240 }} />}>
+      <Card.Meta
+        title={
+          <>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
               <div style={{ display: "flex", alignItems: "center" }}>
                 {name}
               </div>
               <Statistic value={price} prefix="£" precision={2} />
             </div>
-          }
-          // description={description}
-          // style={{ height: 60 }}
-        />
-      </Card>
-    </Link>
+            <AddToBasket id={id} />
+          </>
+        }
+      />
+    </Card>
   );
 };
 
