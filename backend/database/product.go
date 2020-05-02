c19c62cfe018ce91cfdff9dd7ed016865d1db36a
Corin Mulliss <corin.mulliss@gmail.com>
2020-05-02 14:07:18 +0100

    Load products from database

--- backend/database/product.go
+++ backend/database/product.go
@@ -11,6 +11,20 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
+// GetProductsFromJSON returns a list of all stored products stored in a hard-coded JSON file
+func GetProductsFromJSON() []models.Product {
+	data, err := ioutil.ReadFile("./database/products.json")
+	if err != nil {
+		fmt.Print(err)
+	}
+	var products []models.Product
+	err = json.Unmarshal(data, &products)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return products
+}
+
 // GetProducts returns a list of all stored products
 func GetProducts() []models.Product {
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
@@ -20,16 +34,23 @@ func GetProducts() []models.Product {
 	}
 	defer conn.Close(context.Background())
 
-	data, err := ioutil.ReadFile("./database/products.json")
-	if err != nil {
-		fmt.Print(err)
-	}
-
 	var products []models.Product
 
-	err = json.Unmarshal(data, &products)
+	rows, err := conn.Query(context.Background(), "SELECT id, name, price, description, image, rating from products")
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Fprintf(os.Stderr, "Query failed: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var product models.Product
+		err = rows.Scan(&product.ID, &product.Name, &product.Price, &product.Description, &product.Image, &product.Rating)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "rows.Scan failed: %v\n", err)
+			os.Exit(1)
+		}
+		products = append(products, product)
 	}
 
 	return products
